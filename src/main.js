0bb2589d3e4fe54d2dc8e84a91a1f79e9f26e90b
LBussy <lee@bussy.org>
2023-03-24 17:29:40 -0500

    Try again to add FA

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { LoadingPlugin } from "vue-loading-overlay";
 import './style.css'
 import 'vue-loading-overlay/dist/css/index.css';
-import ConfigApp from './App.vue'
-// Bootstrap
+import App from './App.vue'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-// Local stuff
-import './custom.css'
 
 const pinia = createPinia();
 
-const configapp = createApp(ConfigApp);
-configapp.use(FontAwesomeIcon);
-configapp.use(pinia);
-configapp.use(LoadingPlugin);
-configapp.mount('#configapp');
-
-import { library } from "@fortawesome/fontawesome-svg-core";
+// Font Awesome:
+//
+// Import the fontawesome core
+import { library } from '@fortawesome/fontawesome-svg-core'
+// Import font awesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Import specific icons
+import { faTowerBroadcast } from "@fortawesome/free-solid-svg-icons";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faTowerBroadcast } from "@fortawesome/free-solid-svg-icons";
-
+/* Add icons to the library */
 library.add(faBook);
 library.add(faGithub);
 library.add(faTowerBroadcast);
+
+const app = createApp(App);
+app.use(pinia);
+app.use(LoadingPlugin);
+app.use(FontAwesomeIcon);
+app.mount('#app');
