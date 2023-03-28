f18ee9cb9422119e8c93d696f74968eab0df352a
John <thorrak@users.noreply.github.com>
2023-03-28 18:11:00 -0400

    clean up proxy

--- vite.config.js
+++ vite.config.js
@@ -28,41 +28,17 @@ export default defineConfig({
     }
   },
   server: {
-    open: '/config',
+    open: '/wspr/',
     proxy: {
-      '/wspr_ini.php': {
+      '/wspr/wspr_ini.php': {
         target: localServer,
         changeOrigin: true,
         secure: false,
-        rewrite: function (path, req) { return path.replace('/', '/wspr/') },
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
       },
-      '/wspr_log.php': {
+      '/wspr/wspr_log.php': {
         target: localServer,
         changeOrigin: true,
         secure: false,
-        rewrite: function (path, req) { return path.replace('/', '/wspr/') },
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
       },
     },
   }
