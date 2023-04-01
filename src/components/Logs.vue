1ff5a9ff08f601eba506d76e240d19f8637730ab
John <thorrak@users.noreply.github.com>
2023-04-01 08:58:43 -0400

    Watch prop to trigger refresh on prop change

--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -14,8 +14,6 @@
 </template>
 
 <script>
-import {toRef} from "vue"; // TODO?
-
 let interval = 5000; // Log refresh interval
 
 import { useLogMessageStore } from "@/stores/LogStore";
@@ -39,19 +37,41 @@ export default {
       LogMessageStore: useLogMessageStore(),
     }
   },
+  watch: {
+    logFile: function(newLogFile, oldLogFile) {
+      console.log("DEBUG: Triggered: watch: logFile: function(" + newLogFile + ", " + oldLogFile + ")");
+      this.LogMessageStore.logFile = newLogFile;
+
+      // Option A (part 1): Clear the existing interval, and set a brand new interval after forcing the refresh
+      if(this.intervalObject===null) {
+        console.log("DEBUG: interval is not null, clearing");
+        clearInterval(this.intervalObject);
+      }
+
+      // Option B: Leave the interval running, only change the log file (but force a refresh here)
+      let loader = this.$loading.show({});
+      this.LogMessageStore.getLogMessages().then(() => {
+        loader.hide();
+        // ...technically, setting the interval should be done here in Option A to ensure it doesn't overlap with the refresh
+      });
+
+      // Option A (part 2): Actually set the brand new interval
+      this.intervalObject =  setInterval(() => {
+        this.LogMessageStore.getLogMessages();
+      }, interval);
+    }
+  },
   mounted() {
     // Show loader
     let loader = this.$loading.show({});
     // Switch to proper log
     this.LogMessageStore.logFile = this.logFile;
-    //this.LogMessageStore.logFile = toRef(this.props, 'logFile'); // TODO?
     // Get initial data
     this.LogMessageStore.getLogMessages().then(() => {
       loader.hide();
     });
     // Set up periodic refreshes
     this.intervalObject =  setInterval(() => {
-      this.LogMessageStore.logFile = this.logFile;
       this.LogMessageStore.getLogMessages();
     }, interval);
   },
