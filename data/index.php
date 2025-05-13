ab49f418adbb07ff4a27e09eae6353be81f4653a
lbussy <lee@bussy.org>
2025-05-13 16:03:04 -0500

    Move system action modal to template

--- data/index.php
+++ data/index.php
@@ -500,34 +500,13 @@
                         </div>
                     </div>
 
-                    <!-- System action modal -->
-                    <div class="modal fade" id="systemModal" tabindex="-1" aria-labelledby="systemModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="systemModalLabel">System Event</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-
-                                <div id="systemModalBody" class="modal-body">
-                                    <!-- JS will fill this in -->
-                                </div>
-
-                                <!-- Use Bootstrap’s modal-footer class for proper spacing & alignment -->
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary exit-btn" data-bs-dismiss="modal">Exit</button>
-                                    <button type="button" class="btn btn-primary reload-btn" disabled>Reload Page</button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
                 </form>
             </div>
         </div>
     </div>
+
+    <!-- System action modal -->
+    <?php require_once 'system_action_modal.php'; ?>
 
     <!-- Static page footer -->
     <?php require_once 'footer.php'; ?>
