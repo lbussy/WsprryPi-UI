cafcc84ae21f24cfac2bf812e691dc3bc1201e9d
LBussy <lee@bussy.org>
2025-05-16 09:42:54 -0500

    Fix template

--- data/template.php
+++ data/template.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-bs-theme="light">
+<html lang="en" data-bs-theme="auto">
 
 <head>
     <!-- Bootswatch, Boostrap, and Fontawesome, included here: -->
@@ -13,9 +13,33 @@
 </head>
 
 <body>
+    <!-- Fixed Navbar -->
     <?php require_once 'navbar.php'; ?>
 
-    <!-- Main Content Goes Here -->
+    <!-- Main Content -->
+    <div class="container my-5">
+        <!-- TODO: Rename this card from template-card -->
+        <div class="card shadow-sm template-card mt-5">
+
+            <div class="card-header d-flex flex-wrap justify-content-between align-items-center">
+                <!-- Card Title -->
+                <span id="spotsFor">Recent spots for: </span>
+
+                <!-- Reboot, Shutdown and Clocks -->
+                <?php require_once 'clock_and_reboot.php'; ?>
+            </div>
+
+            <!-- Card Body -->
+            <div class="card-body tab-content bg-body">
+                <!-- Card body goes here -->
+            </div>
+
+            <!-- Hidden fieldset to hold settings -->
+            <div id="server-settings" class="d-none">
+                <input type="text" id="callsign" name="callsign" value="" />
+            </div>
+        </div>
+    </div>
 
     <!-- System action modal -->
     <?php require_once 'system_action_modal.php'; ?>
@@ -29,7 +53,7 @@
     <!-- Main JavaScript -->
     <script src="site.js"></script>
 
-    <!-- This page's JavaScript -->
+    <!-- Index JavaScript -->
     <script src="template.js"></script>
 </body>
 
